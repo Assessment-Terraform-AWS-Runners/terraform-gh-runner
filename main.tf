50a24a2e58a8f3d299fa886157cd7d9356ce7eab
Harsh <harsh.s3105@gmail.com>
2026-01-03 21:36:05 +0530

    Updated fiel in terraform for dynamic parts

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
+# Get latest Amazon Linux 2 AMI
 data "aws_ami" "amazon_linux" {
   most_recent = true
-
-  owners = ["amazon"]
+  owners      = ["amazon"]
 
   filter {
     name   = "name"
@@ -14,12 +14,25 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get subnets from default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
 resource "aws_security_group" "gh_runner_sg" {
   name        = "gh-runner-sg"
-  description = "Allow SSH and outbound access"
+  description = "Security group for GitHub Runner EC2"
+  vpc_id      = data.aws_vpc.default.id
 
   ingress {
-    description = "SSH access"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -32,12 +45,20 @@ resource "aws_security_group" "gh_runner_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "github-runner-sg"
+  }
 }
 
 resource "aws_instance" "gh_runner" {
+  count = var.create_ec2 ? 1 : 0
+
   ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
   key_name      = var.key_name
+
+  subnet_id = data.aws_subnets.default.ids[0]
 
   vpc_security_group_ids = [
     aws_security_group.gh_runner_sg.id
--- variables.tf
+++ variables.tf
@@ -1,13 +1,22 @@
 variable "aws_region" {
   description = "AWS region"
+  type        = string
   default     = "ap-south-1"
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
+  type        = string
   default     = "t2.micro"
 }
 
 variable "key_name" {
-  description = "EC2 key pair name for SSH"
+  description = "EC2 SSH key pair name"
+  type        = string
+}
+
+variable "create_ec2" {
+  description = "Whether to create a new EC2 instance"
+  type        = bool
+  default     = true
 }
