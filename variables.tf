86add9a2c9b91d68e7819920a90cf9d8e2607502
Harsh <harsh.s3105@gmail.com>
2026-01-04 00:08:37 +0530

    Add variables to control GitHub runner installation

--- variables.tf
+++ variables.tf
@@ -20,3 +20,31 @@ variable "create_ec2" {
   type        = bool
   default     = true
 }
+
+variable "install_runners" {
+  description = "Install GitHub runners on the EC2 instance"
+  type        = bool
+  default     = false
+}
+
+variable "github_repo_url" {
+  description = "GitHub repository or organization URL"
+  type        = string
+}
+
+variable "github_runner_token" {
+  description = "GitHub runner registration token"
+  type        = string
+  sensitive   = true
+}
+
+variable "runner_count" {
+  description = "Number of runners to install on EC2"
+  type        = number
+  default     = 1
+}
+
+variable "private_key_path" {
+  description = "Path to private key for SSH"
+  type        = string
+}
